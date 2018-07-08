01cd8b130609a598ad94e00986828d37c20f0084
Elijah-Walker <Elijah.Walker747@gmail.com>
2018-07-08 16:04:41 -0600

    basic working app

---
+++ app/data/friends.js
@@ -0,0 +1,105 @@
+// data of friends
+
+var friends = [
+    {
+      name: "Adrian	Mcbride",
+      photo: "https://randomuser.me/api/portraits/women/9.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Glenn	Simpson",
+      photo: "https://randomuser.me/api/portraits/women/15.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Beverly Henderson",
+      photo: "https://randomuser.me/api/portraits/women/44.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Evelyn	Perez",
+      photo: "https://randomuser.me/api/portraits/women/1.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Derrick Gonzalez",
+      photo: "https://randomuser.me/api/portraits/men/81.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Sandra	Moran",
+      photo: "https://randomuser.me/api/portraits/women/8.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
